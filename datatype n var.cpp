70318489289f9f56e5140f5d9a6c8c615df60bfb
Divanshujain1 <jainsujal883@gmail.com>
2026-03-22 11:15:01 +0530

    datatype and varchar

---
+++ datatype n var.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int a=123;
+    cout<<a<<endl;
+    int b=3;
+    cout<<b << endl;
+    int sum = a+b;
+    cout<<"sum : "<<sum<<endl;
+    int size = sizeof(a);
+    cout<<"size of a : "<<size << endl;
+    return 0;
+}
---
+++ helloworld.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main() {
+    cout<<"hello world "<<endl;
+    cout<<"welcome to DSA ";
+    cout<<"\n" ;
+    cout<<"welcome to c++ programming ";
+     cout<<"\n" ;
+    cout << "welcome to c++ programming " << endl ;
+
+   
+    return 0;
+}
