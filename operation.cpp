e576bd4f65b87f274821de49380b5c15b68b8884
Divanshujain1 <jainsujal883@gmail.com>
2026-03-22 16:57:43 +0530

    basic concepts

---
+++ operation.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int a,b , sum ;
+    cin>>a,b ;
+    sum =a+b;
+    cout<<"sum: "<<sum<<endl;
+    int sub = a-b;
+    cout<<"sub: "<<sub<<endl;
+    float c= 2/5;
+    cout<<c << endl;
+    return 0;
+
+}
---
+++ relationalandlogicaloperator.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a,b;
+    cin>>a,b;
+    bool first = (a==b);
+    cout<<"first: "<<first<<endl;
+      bool second = (a<b);
+    cout<<"second: "<<second<<endl;
+      bool third = (a>b);
+    cout<<"third: "<<third<<endl;
+      bool fourth= (a<=b);
+    cout<<"fourth: "<<fourth<<endl;
+      bool five = (a!=b);
+    cout<<"five: "<<five<<endl;
+      bool six = (a>=b);
+    cout<<"six: "<<six<<endl;
+
+
+
+    int c = 2;
+    int d=10;
+    cout<<(  c< d && c<b)<< endl;
+
+
+    return 0;
+
+
+
+    
+
+}
